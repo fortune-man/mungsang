d4659e1c4ecc29f9aeacce6f86c243d6f2592a6a
fortune-man <wnguddl96@knou.ac.kr>
2024-11-24 14:43:17 +0900

    docs: 프로젝트 구조 설계

---
+++ src/main/java/com/example/mungsang/MungsangApplication.java
@@ -0,0 +1,14 @@
+package com.example.mungsang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungsangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungsangApplication.class, args);
+
+	}
+
+}
