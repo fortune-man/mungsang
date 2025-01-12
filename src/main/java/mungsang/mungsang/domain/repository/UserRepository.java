73c08ad0d175047264c8b7e5d4fa83e7a3ba22da
fortune-man <wnguddl96@knou.ac.kr>
2025-01-12 15:03:54 +0900

    feat(UserRepository) : 데이터베이스와 상호작용하는 JPA repository 추가

---
+++ src/main/java/mungsang/mungsang/domain/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package mungsang.mungsang.domain.repository;
+
+import java.util.Optional;
+import mungsang.mungsang.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
