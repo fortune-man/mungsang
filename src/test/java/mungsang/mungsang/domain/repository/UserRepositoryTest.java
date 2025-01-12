d8c8cd2700a9d73cbae9e5f744a8289d35f1c2b5
fortune-man <wnguddl96@knou.ac.kr>
2025-01-12 20:22:46 +0900

    test(UserRepositoryTest) : 회원 저장 테스트 추가

--- src/test/java/mungsang/mungsang/domain/repository/UserRepositoryTest.java
+++ src/test/java/mungsang/mungsang/domain/repository/UserRepositoryTest.java
@@ -1,4 +1,35 @@
+package mungsang.mungsang.domain.repository;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import mungsang.mungsang.domain.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실제 db 사용시
 class UserRepositoryTest {
-  
+  @Autowired
+  private UserRepository userRepository;
+
+  @DisplayName("회원 저장 테스트")
+  @Test
+  public testSaveUser{
+    //given
+    User user = new User();
+    user.setName("김주형");
+    user.setEmail("test@example.com");
+
+    //when
+    User savedUser = userRepository.save(user);
+
+    //then
+    assertNotNull(savedUser.getId());
+    assertEquals("김주형", savedUser.getName());
+    assertEquals("wnguddl96@naver.com", savedUser.getEmaiL());
+  }
+
 }
