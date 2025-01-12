4806cf0c08d85e638de4a38bea3b5b1769275922
fortune-man <wnguddl96@knou.ac.kr>
2025-01-12 20:54:35 +0900

    test(UserControllerTest) : api 요청과 응답이 올바르게 동작하는지 확인하고자 http 요청을 시뮬레이션 하고 비즈니스 로직 대신 mock 데이터를 사용하는 컨트롤러 테스트 추가

---
+++ src/test/java/mungsang/mungsang/controller/UserControllerTest.java
@@ -0,0 +1,44 @@
+package mungsang.mungsang.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import mungsang.mungsang.domain.entity.User;
+import mungsang.mungsang.domain.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private UserService userService;
+
+  @Test
+  void testCreateUser() throws Exception {
+    // given
+    User user = new User();
+    user.setName("API user");
+    user.setEmail("api@example.com");
+    when(userService.createUser(any(User.class))).thenReturn(user);
+
+    // when
+    mockMvc.perform(post("/users"))
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"name\": \"API User\", \"email\": \"api@example.com\"}")
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name").valie("API User"))
+        .andExpect(jsonPath("$.email").value("api@example.com"));
+
+    // then
+  }
+}
