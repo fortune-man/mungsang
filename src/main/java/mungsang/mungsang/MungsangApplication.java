1d524a23636198785a3751b446fead39a917db43
fortune-man <wnguddl96@knou.ac.kr>
2025-01-12 12:33:47 +0900

    init commit - java버전 설정 완료 후 로컬 프로젝트 실행 확인까지 완료

---
+++ src/main/java/mungsang/mungsang/MungsangApplication.java
@@ -0,0 +1,14 @@
+package mungsang.mungsang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+MungsangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungsangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mungsang/mungsang/MungsangApplicationTests.java
@@ -0,0 +1,13 @@
+package mungsang.mungsang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungsangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
