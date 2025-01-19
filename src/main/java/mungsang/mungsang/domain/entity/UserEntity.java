0fb1fe23864aac97e8515763d619a9c3ea6c0b96
fortune-man <wnguddl96@knou.ac.kr>
2025-01-19 13:41:43 +0900

    feat(UserRepository) : userentity를 user로 변환하는 유틸 추가

--- src/main/java/mungsang/mungsang/domain/entity/UserEntity.java
+++ src/main/java/mungsang/mungsang/domain/entity/UserEntity.java
@@ -1,21 +1,21 @@
 package mungsang.mungsang.domain.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 // DB와 직접 매핑되는 JPA 엔티티 역할
 public class UserEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String username;
   private String email;
 
+
   // JPA에서 필요한 기본 생성자
   public UserEntity() {}
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
   public Long getId() {
     return id;
@@ -31,5 +31,13 @@ public class UserEntity {
 
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 }
--- src/main/java/mungsang/mungsang/domain/service/UserService.java
+++ src/main/java/mungsang/mungsang/domain/service/UserService.java
@@ -1,6 +1,8 @@
 package mungsang.mungsang.domain.service;
 
 import mungsang.mungsang.domain.dto.UserResponse;
+import mungsang.mungsang.domain.entity.User;
+import mungsang.mungsang.domain.entity.UserEntity;
 import mungsang.mungsang.domain.repository.UserRepository;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
   private final UserRepository userRepository;
 
-  public UserResponse getUser(Long id) {
-    return userRepository.findById(id)
-        .map(UserRepository::fromEntity)
-        .orElseThrow(() -> new NotFoundException("조회 되지 않는 회원입니다."));
-  }
+
 
 }
